0cdf5f72d4741f8880ae0636b15f195e4be1f17c
JOSE LUIS MU ORIZAGA <jlmuorizaga@gmail.com>
2024-06-11 10:45:21 -0600

    Actualiza conexion_data_db.js

--- captura-ppp-chp-movil-servicios/conexion_data_db.js
+++ captura-ppp-chp-movil-servicios/conexion_data_db.js
@@ -4,11 +4,17 @@
  const DB_PASSWORD = process.env.DB_PASSWORD || 'cheesepizza2001';
  const DB_NAME = process.env.DB_NAME || 'chppreciosespecprodpromocdb';
  
+ //AWS
+ //const DB_PORT = process.env.DB_PORT || 5432;
+
  //MacBook
  //const DB_PORT = process.env.DB_PORT || 5432;
 
  //Laptop Omen
-const DB_PORT = process.env.DB_PORT || 5434;
+//const DB_PORT = process.env.DB_PORT || 5434;
+
+//Desktop INEGI
+const DB_PORT = process.env.DB_PORT || 5433;
 
  module.exports={
     DB_HOST, DB_USER,DB_PASSWORD, DB_NAME,DB_PORT
