478ebf515db6be135aa835fde9099799b5ceceb7
JOSE LUIS MU ORIZAGA <jlmuorizaga@gmail.com>
2024-06-11 11:10:05 -0600

    Update conexion_data_db.js

--- captura-ppp-chp-movil-servicios/conexion_data_db.js
+++ captura-ppp-chp-movil-servicios/conexion_data_db.js
@@ -1,5 +1,5 @@
-//const DB_HOST = process.env.DB_HOST || 'database-1.cgujpjkz4fsl.us-west-1.rds.amazonaws.com';
- const DB_HOST = process.env.DB_HOST || 'localhost';
+const DB_HOST = process.env.DB_HOST || 'database-1.cgujpjkz4fsl.us-west-1.rds.amazonaws.com';
+ //const DB_HOST = process.env.DB_HOST || 'localhost';
  const DB_USER = process.env.DB_USER || 'cheesepizzauser';
  const DB_PASSWORD = process.env.DB_PASSWORD || 'cheesepizza2001';
  const DB_NAME = process.env.DB_NAME || 'chppreciosespecprodpromocdb';
