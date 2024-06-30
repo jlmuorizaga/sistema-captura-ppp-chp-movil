9a5b0cdd5738f3f9ed36d061387137147d3e3158
jlmuorizaga <jlmuorizaga@gmail.com>
2024-06-30 15:21:39 -0600

    Sincronizar primera vez

--- captura-ppp-chp-movil-sistema/src/app/home/home.page.ts
+++ captura-ppp-chp-movil-sistema/src/app/home/home.page.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButtons,IonMenu,IonMenuButton} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonButtons, IonHeader, IonToolbar, IonTitle, IonContent,IonMenu,IonMenuButton],
 })
 export class HomePage {
   constructor() {}
